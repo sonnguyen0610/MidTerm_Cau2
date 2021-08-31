4c19bce450e3c7d200fc934a98a5b1a2e3cc15cd
sonnguyen0610 <17130194@st.hcmuaf.edu.vn>
2021-08-31 11:30:10 +0700

    - Check pass

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Nhap mat khau: ");
-        String s=sc.nextLine();
-        Cau2 cau2=new Cau2();
-        if(cau2.checkLenght(s)){
-            if(cau2.checkUpper(s)){
-                if(cau2.checkNumber(s)){
-                    System.out.println("Mk oke");
-                }else {
-                    System.out.println("Mat khau chua du manh");
+        String s = sc.nextLine();
+        Cau2 cau2 = new Cau2();
+        if (cau2.checkLenght(s)) {
+            if (cau2.checkUpper(s)) {
+                if (cau2.checkNumber(s)) {
+                    if (cau2.checkSpecial(s)) {
+                        System.out.println("Strong passwork");
+                    } else {
+                        System.out.println("Passwork is not enough strong");
+                    }
+                } else {
+                    System.out.println("Passwork is not enough strong");
                 }
-            }else {
-                System.out.println("Mat khau chua du manh");
+            } else {
+                System.out.println("Passwork is not enough strong");
             }
-        }else {
-            System.out.println("Mat khau yeu");
+        } else {
+            System.out.println("Weak passwork");
         }
     }
 }
